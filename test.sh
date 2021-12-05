af53dbebc15ea2f32612ca688f5a173fda3e95aa
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-12-05 20:32:10 +0900

    ほぼ完成

--- test.sh
+++ test.sh
@@ -2,44 +2,46 @@
 
 set -e
 
-function pushCommit (){
+function push_commit (){
   for i in `seq $1`;do
     echo 'hoge' >> ./hoge.txt
     git add ./hoge.txt
-    git commit -m "$(gdate +"%Y-%m-%d"):$i 回目の自動コミット"
+    git commit -m "$(gdate +"%Y-%m-%d"):$i 回目の自動プッシュ"
+    git push
   done
-  git push
 }
 
-function evalCommitDate (){
-  commitDatetime=$1
-  commitDatetime=`echo ${commitDatetime%Z}`
-  commitDatetime=`echo ${commitDatetime//T/ }`
-  result=$(gdate -d "9 hours $commitDatetime" +"%Y-%m-%d")
+function eval_push_date (){
+  local commit_datetime=$1; local result
+  commit_datetime=`echo ${commit_datetime%Z}`
+  commit_datetime=`echo ${commit_datetime//T/ }`
+  result=$(gdate -d "9 hours $commit_datetime" +"%Y-%m-%d")
   echo $result
 }
 
-declare -i todayCommitCount=0
+declare -i today_push_count=0
+readonly PUSH_QUOTA_COUNT=5
 
 json=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/ryo-takaya/events | jq)
 len=$(echo $json | jq length)
 
 for i in $( seq 0 $(($len - 1)) ); do
   row=`echo ${json} | jq .[${i}]`
-  eventType=`echo ${row} | jq -r .type`
+  event_type=`echo ${row} | jq -r .type`
 
-  if [ ${eventType} = "PushEvent" ]; then
-    createdAt=`echo ${row} | jq -r .created_at`
-    evalCommitDate $createdAt
-    commitTime=`evalCommitDate $createdAt`
+  if [ ${event_type} = "PushEvent" ]; then
+    created_datetime=`echo ${row} | jq -r .created_at`
+    push_date=`eval_push_date $created_datetime`
     today=$(gdate +"%Y-%m-%d")
-    if [ "$commitTime" = "$today" ]; then
-      todayCommitCount=$((todayCommitCount+1))
+    if [ "$push_date" = "$today" ]; then
+      today_push_count=$((today_push_count+1))
     fi
   fi
 done
 
-additionCommitCount=$((6-todayCommitCount))
-if [ $additionCommitCount -gt 0 ]; then
-  pushCommit $additionCommitCount
+additional_push_count=$((PUSH_QUOTA_COUNT-today_push_count))
+if [ $additional_push_count -gt 0 ]; then
+  push_commit $additional_push_count
+else
+  echo "今日は既に$PUSH_QUOTA_COUNT 回のプッシュをしています"
 fi
