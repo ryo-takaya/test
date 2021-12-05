d25a7a6eabf0051597d7ea7ffecdc6b4981d0a33
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-12-05 19:46:09 +0900

    最新コミット

--- test.sh
+++ test.sh
@@ -1,18 +1,45 @@
 #!/usr/bin/env bash
 
+set -e
+
+function pushCommit (){
+  for i in `seq $1`;do
+    echo 'hoge' >> ./hoge.txt
+    git add ./hoge.txt
+    git commit -m "$(gdate +"%Y-%m-%d"):$i 回目の自動コミット"
+  done
+  git push
+}
+
+function evalCommitDate (){
+  commitDatetime=$1
+  commitDatetime=`echo ${commitDatetime%Z}`
+  commitDatetime=`echo ${commitDatetime//T/ }`
+  result=$(gdate -d "9 hours $commitDatetime" +"%Y-%m-%d")
+  echo $result
+}
+
+declare -i todayCommitCount=0
+
 json=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/ryo-takaya/events | jq)
 len=$(echo $json | jq length)
 
 for i in $( seq 0 $(($len - 1)) ); do
   row=`echo ${json} | jq .[${i}]`
   eventType=`echo ${row} | jq -r .type`
-  if [ "$eventType" = "PushEvent" ]; then
-        createdAt=`echo ${row} | jq -r .created_at`
-        format=${createdAt:0:10}
-        today=$(date  +"%Y-%m-%d")
-        if [ "$format" = "$today" ]; then
-          echo ${format} ${today}
-        fi
-  fi
 
+  if [ ${eventType} = "PushEvent" ]; then
+    createdAt=`echo ${row} | jq -r .created_at`
+    evalCommitDate $createdAt
+    commitTime=`evalCommitDate $createdAt`
+    today=$(gdate +"%Y-%m-%d")
+    if [ "$commitTime" = "$today" ]; then
+      todayCommitCount=$((todayCommitCount+1))
+    fi
+  fi
 done
+
+additionCommitCount=$((6-todayCommitCount))
+if [ $additionCommitCount -gt 0 ]; then
+  pushCommit $additionCommitCount
+fi
